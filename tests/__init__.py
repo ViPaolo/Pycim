ecd80d028a067b0ac68d2fb09caacab0923baae8
ViPaolo <137789515+ViPaolo@users.noreply.github.com>
2025-02-05 17:54:58 +0100

    Update __init__.py
    Translated the ImportError.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,7 @@ try:
     import os
     import sys
 except ImportError as e:
-    raise ImportError(f"Dipendenze mancanti: {e}. Installa i requisiti con 'pip install -r requirements.txt'")
+    raise ImportError(f"Missing dependency: {e}. Install requirements with 'pip install -r requirements.txt'")
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
